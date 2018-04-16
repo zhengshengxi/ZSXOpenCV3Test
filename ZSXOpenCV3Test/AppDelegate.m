c930582d6d3e4438c8f4507ca4c00c4b5fa85906
yh-zsx <314334043@qq.com>
2018-04-16 11:59:55 +0800

    1

--- ZSXOpenCV3Test/AppDelegate.m
+++ ZSXOpenCV3Test/AppDelegate.m
@@ -17,7 +17,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
-    //哈哈嘿嘿呼呼
+    //啦啦啦
     return YES;
 }
 
